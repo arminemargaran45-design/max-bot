00512f2302a2bd6f64f91d26f6d4f2dc8bb0c3ca
arminemargaran45-design <arminemargaran45@gmail.com>
2026-05-26 13:57:05 +0300

    fix max response

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
-const TOKEN = 'f9LHodD0cOL42IwnzjW1CaXuuJvqgnaPXfkR0tSYUBj0Duq8-cEfuvvGcVeCdpkiI_AoJDCLkR_TDjxsZLGY';
-
-app.get('/', (req, res) => {
-    res.send('MAX BOT WORKING');
-});
-
 app.post('/webhook', async (req, res) => {
 
     console.log('Сообщение от MAX:');
     console.log(JSON.stringify(req.body, null, 2));
 
     const message =
-        '👋 Добро пожаловать в МедАльянс\n\n' +
         'Выберите раздел:\n\n' +
         '1. Новости\n' +
         '2. График\n' +
@@ -24,14 +11,10 @@ app.post('/webhook', async (req, res) => {
         '4. Задачи\n' +
         '5. Помощь';
 
-    res.json({
-        text: message
+    res.status(200).json({
+        message: {
+            text: message
+        }
     });
 
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
 });
