2d91a918b1eddced9f92b12eaa1e9c212d153c82
arminemargaran45-design <arminemargaran45@gmail.com>
2026-05-26 14:04:04 +0300

    max api

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+const TOKEN = 'f9LHodD0cOL42IwnzjW1CaXuuJvqgnaPXfkR0tSYUBj0Duq8-cEfuvvGcVeCdpkiI_AoJDCLkR_TDjxsZLGY';
+
+app.get('/', (req, res) => {
+    res.send('MAX BOT WORKING');
+});
+
 app.post('/webhook', async (req, res) => {
 
     console.log('Сообщение от MAX:');
     console.log(JSON.stringify(req.body, null, 2));
 
-    const message =
-        'Выберите раздел:\n\n' +
-        '1. Новости\n' +
-        '2. График\n' +
-        '3. Контакты\n' +
-        '4. Задачи\n' +
-        '5. Помощь';
+    try {
 
-    res.status(200).json({
-        message: {
-            text: message
-        }
-    });
+        const chatId = req.body.chat_id;
 
+        await axios.post(
+            'https://botapi.max.ru/messages',
+            {
+                chat_id: chatId,
+                text:
+                    'Выберите раздел:\n\n' +
+                    '1. Новости\n' +
+                    '2. График\n' +
+                    '3. Контакты\n' +
+                    '4. Задачи\n' +
+                    '5. Помощь'
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+    } catch (error) {
+        console.log(error.response?.data || error.message);
+    }
+
+    res.sendStatus(200);
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
